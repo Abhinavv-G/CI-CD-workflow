2f97cd013c58b447e773bcab6be926ac46ee57da
abhinav1412 <abhinavgautam3917@gmail.com>
2026-05-10 17:25:47 +0530

    Added new bombastic api

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, jsonify
 import platform
 import os
+import time
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
+    print("Recieved a Requestzzzz")
     return jsonify({"status": "ok", "message": "System Health API is running"})
 
 @app.route("/health")
@@ -20,6 +22,23 @@ def info():
         "python_version": platform.python_version(),
         "hostname": platform.node()
     })
+
+
+@app.route("/get_ma_infoz")
+def get_ma_infoz():
+    return jsonify({
+        "namez": "shank",
+        "gamez": "devops"
+    })
+
+
+@app.route("/random-image")
+def random_image():
+    return jsonify({
+        "image_url": f"https://picsum.photos/400?seed={time.time_ns()}"
+    })
+
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
